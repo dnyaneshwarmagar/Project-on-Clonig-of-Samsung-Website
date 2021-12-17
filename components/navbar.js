037d54061eb6b95f4bd59d5bcb2b9ad40d3a1a3b
dnyaneshwarmagar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2021-12-17 22:29:30 +0530

    committing  u3A project
    my commit

---
+++ components/navbar.js
@@ -0,0 +1,24 @@
+function navbar(){
+    return ` <div id="nav">
+    <div class="set1"><img src=".\image\samsungLogo.png" id="logo"></img></div>
+    <div class="set2">
+        <a href="#">Mobile</a>
+        <a href="#">TV&AV</a>
+        <a href="#">Home Appliances</a>
+        <a href="#">Computing</a>
+        <a href="#">Displays</a>
+        <a href="#">Offers</a>
+    </div>
+    <div class="set3">
+        <a href="#">Explore</a>
+        <a href="#">Support</a>
+        <a href="#">For Business</a>
+    </div>
+    <div class="set4">
+        <a href=""><img src=".\image\searchLogo.png" alt=""></a>
+        <a href=""><img src=".\image\cart.png" alt=""></a>
+        <a href=""><img src=".\image\login.jpg" alt=""></a>
+    </div>
+</div>`
+}
+export default navbar;
